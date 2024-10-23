ae2bce759ce7a4feb5bcb99a1595ab22bef31b18
Abhishek Swami <abhishekswami1435@gmail.com>
2024-10-23 19:03:39 +0530

    Add: all task

---
+++ src/components/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='alltask' className='bg-[#1c1c1c] p-5 rounded mt-5 flex flex-col gap-2 overflow-auto h-40'>
+        <div className='bg-red-400 py-2 px-4 flex justify-between rounded'>
+            <h2>Abhishek</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 py-2 px-4 flex justify-between rounded'>
+            <h2>Abhishek</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 py-2 px-4 flex justify-between rounded'>
+            <h2>Abhishek</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-emerald-400 py-2 px-4 flex justify-between rounded'>
+            <h2>Abhishek</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 flex justify-between rounded'>
+            <h2>Abhishek</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header";
 import CreateTask from "../CreateTask";
+import AllTask from "../AllTask";
 
 const AdminDashboard = () => {
     return (
         <div className="p-10 h-screen w-full">
             <Header />
             <CreateTask />
+            <AllTask />
         </div>
     );
 };
